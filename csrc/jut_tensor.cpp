c257f39dfea1ca82ceae8cc555d3f6973f0a787b
KDr2 <zhuo@hexoasis.com>
2020-02-25 21:31:14 +0800

    simple grad and operator add

--- csrc/jut_tensor.cpp
+++ csrc/jut_tensor.cpp
@@ -4,27 +4,34 @@
 #include <iostream>
 
 extern "C" {
-    torch::Tensor* tensor_from_data(void *data, int64_t *size_data, size_t dim);
+    // creation and repr
+    torch::Tensor* tensor_from_data(void *data, int64_t *size_data, size_t dim, int grad);
     void tensor_destroy(torch::Tensor *tensor);
-    void print_tensor(torch::Tensor *tensor);
     const char* tensor_to_string(torch::Tensor *tensor);
+
+    // methods on Tensor
+    torch::Tensor* tensor_method_sum(torch::Tensor *t);
+    torch::Tensor* tensor_method_grad(torch::Tensor *t);
+    void tensor_method_backward(torch::Tensor *t);
+
+    // operators
+    torch::Tensor* tensor_op_add(torch::Tensor *a, torch::Tensor *b);
 }
 
-torch::Tensor* tensor_from_data(void *data, int64_t *size_data, size_t dim) {
+
+// creation and repr
+torch::Tensor* tensor_from_data(void *data, int64_t *size_data, size_t dim, int grad) {
     // TODO:
     //  - Deal with element type, now only works for the detault type (float32)
     c10::ArrayRef<int64_t> sizes(size_data, dim);
-    torch::Tensor res = torch::from_blob(data, sizes);
-    return new torch::Tensor(res.clone());
+    // TODO: copy and deleter
+    torch::Tensor res = torch::from_blob(data, sizes, torch::requires_grad(grad));
+    return new torch::Tensor(res);
 }
 
 void tensor_destroy(torch::Tensor *tensor) {
     // std::cout << "DEBUG: Tensor " << tensor << " is destroyed!\n";
     if(tensor) delete tensor;
-}
-
-void print_tensor(torch::Tensor *tensor) {
-    std::cout << *tensor << std::endl;
 }
 
 const char* tensor_to_string(torch::Tensor *tensor) {
@@ -35,4 +42,26 @@ const char* tensor_to_string(torch::Tensor *tensor) {
     memcpy(res, str.c_str(), str.length());
     res[str.length()] = 0;
     return res;
+}
+
+
+// methods on Tensor
+torch::Tensor* tensor_method_sum(torch::Tensor *t) {
+    torch::Tensor sum = t->sum();
+    return new torch::Tensor(sum);
+}
+
+torch::Tensor* tensor_method_grad(torch::Tensor *t) {
+    torch::Tensor g = t->grad();
+    return new torch::Tensor(g);
+}
+
+void tensor_method_backward(torch::Tensor *t) {
+    t->backward();
+}
+
+// operators
+torch::Tensor* tensor_op_add(torch::Tensor *a, torch::Tensor *b) {
+    torch::Tensor sum = *a + *b;
+    return new torch::Tensor(sum);
 }
--- src/JTorch.jl
+++ src/JTorch.jl
@@ -2,7 +2,8 @@ module JTorch
 
 using Libdl
 
-export Tensor
+export Tensor,
+    grad, backward
 
 const PROJECT_DIR = (@__DIR__) |> dirname
 
@@ -12,21 +13,27 @@ end
 
 mutable struct Tensor
     pointer::Ptr
+
+    function Tensor(p::Ptr)
+        ret = new(p)
+        finalizer(ret) do t
+            ccall((:tensor_destroy, :libjtorch),
+                  Ptr{UInt8}, (Ptr{Cvoid},),
+                  t.pointer)
+        end
+        return ret
+    end
+
 end
 
-function Tensor(array::AbstractArray{Float32, N}) where N
+function Tensor(array::AbstractArray{Float32, N}; requires_grad=false) where N
     dims = collect(size(array))
     row_major = permutedims(array, collect(length(dims):-1:1))
+    grad = requires_grad ? 1 : 0
     ptr = ccall((:tensor_from_data, :libjtorch),
-                Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Clonglong}, Csize_t),
-                row_major, dims, length(dims))
-    ret = Tensor(ptr)
-    finalizer(ret) do t
-        ccall((:tensor_destroy, :libjtorch),
-              Ptr{UInt8}, (Ptr{Cvoid},),
-              t.pointer)
-    end
-    return ret
+                Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Clonglong}, Csize_t, Cint),
+                row_major, dims, length(dims), grad)
+    Tensor(ptr)
 end
 
 function Base.string(t::Tensor)
@@ -46,5 +53,38 @@ end
 function Base.display(t::Tensor)
     show(stdout, t)
 end
+
+# methods
+
+function Base.sum(a::Tensor)
+    ptr = ccall((:tensor_method_sum, :libjtorch),
+                Ptr{Cvoid}, (Ptr{Cvoid},),
+                a.pointer)
+    Tensor(ptr)
+end
+
+function grad(a::Tensor)
+    ptr = ccall((:tensor_method_grad, :libjtorch),
+                Ptr{Cvoid}, (Ptr{Cvoid},),
+                a.pointer)
+    Tensor(ptr)
+end
+
+function backward(a::Tensor)
+    ccall((:tensor_method_backward, :libjtorch),
+          Ptr{Cvoid}, (Ptr{Cvoid},),
+          a.pointer)
+    nothing
+end
+
+# operators
+
+function Base.:+(a::Tensor, b::Tensor)
+    ptr = ccall((:tensor_op_add, :libjtorch),
+                Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Cvoid}),
+                a.pointer, b.pointer)
+    Tensor(ptr)
+end
+
 
 end
