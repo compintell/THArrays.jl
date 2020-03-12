afb3ff3ce255781df7ba5080e4815b60b6082211
KDr2 <zhuo@hexoasis.com>
2020-03-12 08:52:00 +0800

    generate functions who return list of `tensor*`

--- src/api_generator.jl
+++ src/api_generator.jl
@@ -23,7 +23,7 @@ const PROJECT_DIR = (@__DIR__) |> dirname
 const CPP_API_FILE = joinpath(PROJECT_DIR, "csrc", "torch_api_generated.cpp.h")
 const JUL_API_FILE = joinpath(PROJECT_DIR, "src", "autogen-methods.jl")
 
-const FUNC_SIG_REG = r"(\w+)\s+\*?atg_(\w+)\((.+)\)\s*{"
+const FUNC_SIG_REG = r"(\w+)\s+(\*?atg_\w+)\((.+)\)\s*{"
 
 const JULIA_KEYWORDS = Set(["function", "end"])
 
@@ -61,7 +61,15 @@ struct APIFunction
     function APIFunction(m::RegexMatch, n::Int)
         csig = strip(m.match, [' ', '{', '\n'])
         args = parse_args(m[3])
-        new(csig, m[2], m[1], n, args)
+        ret_type = m[1]
+        fname = m[2]
+        if fname[1] == '*'
+            fname = fname[6:end]
+            ret_type *= '*'
+        else
+            fname = fname[5:end]
+        end
+        new(csig, fname, ret_type, n, args)
     end
 end
 
@@ -80,20 +88,14 @@ function parse_args(args::AbstractString)
 end
 
 function julia_source(f::APIFunction)
-    if f.output_count > 1000
+    if  length(f.args) < 1
         @warn "E1: can't translate function [$(f.cpp_signature)], ignored."
-        return "# $(f.func_name) ignored"
-    end
-
-    if f.return_type != "void" || length(f.args) < 1 ||
-        f.args[1].second != "tensor*"
-        @warn "E2: can't translate function [$(f.cpp_signature)], ignored."
         return "# $(f.func_name) ignored"
     end
 
     for arg in f.args
         if !haskey(C_TYPE_MAP, arg.second)
-            @warn "E3: can't translate function [$(f.cpp_signature)], ignored."
+            @warn "E2: can't translate function [$(f.cpp_signature)], ignored."
             return "# $(f.func_name) ignored"
         end
     end
@@ -107,16 +109,16 @@ function julia_source(f::APIFunction)
         push!(lines, "export $(f.func_name)")
     end
 
-    output_init = join(repeat(["0"], f.output_count), ", ")
-    para_type = any(x -> x.second == "tensor*", f.args[2:end]) ?
+    start = f.args[1].first == "out__" ? 2 : 1
+    para_type = any(x -> x.second == "tensor*", f.args[start:end]) ?
         " where {T,N}" : ""
+    ccall_ret = start == 1 ? "Ptr{Int}" : "Cvoid"
 
     push!(lines, "function $(f.func_name)($(julia_args(f)))$(para_type)")
-    push!(lines, "    outputs__ = Int[$(output_init)]")
     push!(lines, julia_locals(f))
-    push!(lines, "    ccall((:atg_$(f.func_name), :libtorch_capi),")
-    push!(lines, "          Cvoid, ($(ccall_args(f))),")
-    push!(lines, "          pointer(outputs__), $(ccall_julia_args(f)))")
+    push!(lines, "    __cret = ccall((:atg_$(f.func_name), :libtorch_capi),")
+    push!(lines, "                 $(ccall_ret), ($(ccall_args(f))),")
+    push!(lines, "                 $(ccall_julia_args(f)))")
     push!(lines, return_statement(f))
     push!(lines, "end")
 
@@ -125,7 +127,8 @@ end
 
 function julia_args(f::APIFunction)
     args = []
-    for i in 2:length(f.args)
+    start = f.args[1].first == "out__" ? 2 : 1
+    for i in start:length(f.args)
         p = f.args[i]
         if endswith(p.first, "_len") && endswith(f.args[i-1].first, "_data")
             nothing
@@ -138,49 +141,69 @@ end
 
 function julia_locals(f::APIFunction)
     lines = []
-
-    for i in 2:length(f.args)
+    for i in 1:length(f.args)
         p = f.args[i]
         if endswith(p.first, "_len") && endswith(f.args[i-1].first, "_data")
             push!(lines, "    $(p.first) = length($(f.args[i-1].first))")
         elseif p.second == "scalar"
             push!(lines, "    $(p.first)_s_ = Scalar($(p.first))")
         elseif p.second == "tensor*"
-            push!(lines, "    $(p.first)_ta_ = map(x->x.pointer, $(p.first))")
+            if p.first == "out__"
+                output_init = join(repeat(["0"], f.output_count), ", ")
+                push!(lines, "    outputs__ = Int[$(output_init)]")
+            else
+                push!(lines, "    $(p.first)_ta_ = map(x->x.pointer, $(p.first))")
+            end
         end
     end
     join(lines, "\n")
 end
 
 function ccall_args(f::APIFunction)
-    length(f.args) == 1 && return C_TYPE_MAP[f.args[1].sceond] * ","
+    length(f.args) == 1 && return C_TYPE_MAP[f.args[1].second] * ","
     args = map(p -> C_TYPE_MAP[p.second], f.args)
     join(args, ", ")
 end
 
 function ccall_julia_args(f::APIFunction)
-    args = map(f.args[2:end]) do p
+    args = map(f.args) do p
+        p.second == "tensor*" && p.first == "out__" && return "outputs__"
+        p.second == "tensor*" && return "$(p.first)_ta_"
         p.second == "tensor"  && return "$(p.first).pointer"
         p.second == "scalar"  && return "$(p.first)_s_.pointer"
-        p.second == "tensor*" && return "$(p.first)_ta_"
         return p.first
     end
     join(args, ", ")
 end
 
 function return_statement(f::APIFunction)
-    if f.output_count == 1
-        return "    return tensor_from_ptr(Ptr{Cvoid}(outputs__[1]))"
-    end
+    if f.return_type == "void" && f.args[1].first == "out__"
+        if f.output_count == 1
+            return "    return tensor_from_ptr(Ptr{Cvoid}(outputs__[1]))"
+        end
 
-    lines = []
-    for i in 1:f.output_count
+        lines = []
+        for i in 1:f.output_count
+            push!(lines,
+                  "    __o_$(i) = tensor_from_ptr(Ptr{Cvoid}(outputs__[$(i)]))")
+        end
         push!(lines,
-              "    __o_$(i) = tensor_from_ptr(Ptr{Cvoid}(outputs__[$(i)]))")
+              "    return " * join(map(x-> "__o_$x", 1:f.output_count), ", "))
+        return join(lines, "\n")
+    elseif f.return_type == "tensor*"
+        lines = []
+        push!(lines, "    ptrs__, i__ = Int[], 1")
+        push!(lines, "    while true")
+        push!(lines, "        ptr__ = unsafe_load(__cret, i__)")
+        push!(lines, "        ptr__ == 0 && break")
+        push!(lines, "        push!(ptrs__, ptr__)")
+        push!(lines, "        i__ += 1")
+        push!(lines, "    end")
+        push!(lines, "    ccall(:free, Cvoid, (Ptr{Cvoid},), __cret)")
+        push!(lines, "    return map(x -> tensor_from_ptr(Ptr{Nothing}(x)), ptrs__)")
+        return join(lines, "\n")
     end
-    push!(lines,
-          "    return " * join(map(x-> "__o_$x", 1:f.output_count), ", "))
-    return join(lines, "\n")
+    return ""
 end
 
 
@@ -207,7 +230,7 @@ function main()
             if match(r"out__\[\d+\]\s*=\s*new", line) != nothing
                 output_count += 1
             elseif match(r"out__\[\D+\]\s*=\s*new", line) != nothing
-                output_count += 1001
+                output_count = 1
             end
         end
     end
@@ -221,7 +244,7 @@ function main()
 
     close(output)
 
-    print("$(count) methods generated!\n")
+    @info "$(count) methods generated!\n"
 end
 
 main()
