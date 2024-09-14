21ec9c2eb018f64ead686ab758084f31e1b865d5
KDr2 <zhuo.dev@gmail.com>
2024-09-15 07:20:20 +0800

    fix docs compilation

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,18 @@
 using Documenter, THArrays
 
 makedocs(
-  modules=[THArrays],
-  sitename="THArrays",
-  pages = [
-      "Home"        => "index.md",
-      "Tensor"      => "tensor.md",
-      "AD"          => "ad.md",
-      "TorchScript" => "torchscript.md",
-      "Reference"   => "reference.md",
-  ],
-  format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_EVENT_PATH")))
+    modules=[THArrays],
+    sitename="THArrays",
+    pages = [
+        "Home"        => "index.md",
+        "Tensor"      => "tensor.md",
+        "AD"          => "ad.md",
+        "TorchScript" => "torchscript.md",
+        "Reference"   => "reference.md",
+    ],
+    format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_EVENT_PATH"),
+                             size_threshold_warn = 10 * 1024 * 1024,
+                             size_threshold = 10 * 1024 * 1024))
 
 deploydocs(
     repo = "github.com/compintell/THArrays.jl.git",
